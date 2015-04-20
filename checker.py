f5d329d18257f64f20a7610d7a647db487e0d793
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-20 11:25:24 -0700

    #1 Add queue names

--- checker.py
+++ checker.py
@@ -4,3 +4,7 @@ import os
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 REGION = 'us-west-2'
+
+# Set queue name variables
+COMPOSITE_QUEUE = 'snapsat_composite_queue'
+PREVIEW_QUEUE = 'snapsat_preview_queue'
